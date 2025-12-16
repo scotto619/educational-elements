03ad3f062e4eb9043fb32c0e342ff2c82b135ac9
scotto619 <scotto6190@gmail.com>
2025-12-16 13:29:30 +1000

    stuff

--- pages/api/delete-account.js
+++ pages/api/delete-account.js
@@ -19,6 +19,12 @@ if (!getApps().length) {
 const db = getFirestore();
 const adminAuth = getAuth();
 
+export const config = {
+  api: {
+    bodyParser: true,
+  },
+};
+
 export default async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
